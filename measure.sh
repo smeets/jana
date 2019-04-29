1bde64e089df485c1596208d96e0f07eb0a4634d
Axel Smeets <murlocbrand@gmail.com>
2019-04-29 12:45:12 +0200

    fix measure script

--- measure.sh
+++ measure.sh
@@ -5,10 +5,14 @@ GATEWAY=$(ip route show | grep default | sed -n 's/[^0-9]\+\([0-9\.]\+\).*/\1/p'
 LOCAL=$(ip route get $GATEWAY | sed -n 's/.*src \([0-9\.]\+\)/\1/p')
 
 XYZ=$1
-JANALOG_CSV="logfile-$XYZ.csv"
-CAPTURE_PCAP="capture-$XYZ.pcap"
-CAPTURE_CSV="$(basename "$CAPTURE_PCAP" .pcap).csv"
-FINAL_CSV="data-$XYZ.csv"
+
+rm -rf ./data/$XYZ
+mkdir -p ./data/$XYZ
+
+JANALOG_CSV="$(pwd)/data/$XYZ/logfile.csv"
+CAPTURE_PCAP="$(pwd)/data/$XYZ/capture.pcap"
+CAPTURE_CSV="$(pwd)/data/$XYZ/capture.csv"
+FINAL_CSV="$(pwd)/data/$XYZ/result.csv"
 
 echo "$LOCAL @ $GATEWAY"
 echo "jana     > $JANALOG_CSV"
@@ -18,19 +22,25 @@ echo "final    > $FINAL_CSV"
 
 tshark -F pcap -w $CAPTURE_PCAP -f "udp"  2>/dev/null &
 TPID=$(pgrep tshark)
-# src/jana -c $GATEWAY -f $JANALOG_CSV
+
+sleep 5s
+
+./src/jana -c $GATEWAY -f $JANALOG_CSV
 
 sleep 10s
+kill $TPID > /dev/null 2>&1
 
-while [ 1 ] 
-do    
-    if kill $TPID > /dev/null 2>&1
+while [ 1 ]
+do
+    if ps -p $TPID
     then
         sleep 1
     else
         break
     fi
 done
+
+sleep 10s
 
 ./src/pcap2csv $CAPTURE_PCAP -c $LOCAL -s $GATEWAY > $CAPTURE_CSV
 ./src/calc $CAPTURE_CSV $JANALOG_CSV > $FINAL_CSV
